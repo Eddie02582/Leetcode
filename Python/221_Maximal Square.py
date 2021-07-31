fa8c4a4d3d1f4fe5a9f3dcd35c47962d982e7e6c
Eddie <38853252+Eddie02582@users.noreply.github.com>
2021-07-31 15:58:24 +0800

    Update 221_Maximal Square.py

--- Python/221_Maximal Square.py
+++ Python/221_Maximal Square.py
@@ -30,4 +30,28 @@ class Solution:
                     matrix[i][j] = min(matrix[i][j - 1],matrix[i - 1][j],matrix[i - 1][j - 1]) + 1
                     side = max (side,matrix[i][j])  
         return side *side
-        
+
+    def maximalSquare_bfs(self, matrix: List[List[str]]) -> int:
+        if not any(matrix):
+            return 0
+        m,n = len(matrix),len(matrix[0])
+        from collections import deque
+        max_squre = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == "1":
+                    side = 0
+                    queue = deque([(i,j)])
+                    while queue:
+                        side += 1
+                        max_squre = max(max_squre,side)
+                        length = len(queue)
+                        for _ in range(len(queue)):
+                            x,y = queue.popleft()
+                            if any ( r < 0 or c < 0 or matrix[r][c] != "1" for (r,c) in [(x - 1,y),(x - 1,y - 1),( x , y - 1)]):
+                                queue = []
+                                break                          
+                            queue.extend([(x - 1,y),(x-1,y - 1),(x,y - 1)])
+             
+                    
+        return  max_squre * max_squre    
