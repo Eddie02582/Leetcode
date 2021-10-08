c77d8d62271df271a2c36a626e76f364fa550dc9
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2021-10-08 16:09:58 +0800

    update

---
+++ C++/0014. Longest Common Prefix.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1)
+            return strs[0];
+        sort(strs.begin(),strs.end());
+        int i = 0;       
+        string prefix = "";
+        int size = strs.size();
+        for (int i = 0; i < strs[0].size() & i < strs[size - 1].size() ;i++ ){           
+            if(strs[0][i] != strs[size -1][i])
+                return prefix;
+            
+            prefix += strs[0][i];  
+        }
+        return prefix;
+
+    }
+};
+
+
+
+
