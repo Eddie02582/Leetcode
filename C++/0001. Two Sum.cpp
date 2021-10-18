0ad97686970eb52aa442f5d4fa63524e5c2eceaa
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2021-10-18 11:45:55 +0800

    Update 0001. Two Sum.cpp

--- C++/0001. Two Sum.cpp
+++ C++/0001. Two Sum.cpp
@@ -31,19 +31,6 @@ public:
         }
         return {};          
     }
-
-    vector<int> twoSum_3(vector<int>& nums, int target) {
-        unordered_map<int, int> imap;
-        
-        for (int i = 0;; ++i) {
-            auto it = imap.find(target - nums[i]);
-            
-            if (it != imap.end()) 
-                return vector<int> {i, it->second};
-                
-            imap[nums[i]] = i;
-    }
-
 };
 
 
