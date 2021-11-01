28ac6be11881c1dd77605f9708d2c70da77e4d3f
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2021-11-01 09:57:03 +0800

    update

---
+++ C++/0015. 3Sum.cpp
@@ -0,0 +1,42 @@
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>  ans;  
+        if (nums.size() <3)
+            return ans;
+        sort(nums.begin(),nums.end());
+           
+        
+        for (int i = 0;i < nums.size() - 1;i++){
+            if ( i >0 && nums[i] == nums[i - 1])
+                continue;            
+            int left = i + 1,right = nums.size() - 1;           
+            while (left < right){
+                if(nums[left] + nums[right] + nums[i] == 0){
+                    ans.push_back(vector<int>{nums[i],nums[left],nums[right]});
+                    left += 1;
+                    right -= 1;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left += 1;
+                    
+                    while (left < right && nums[right] == nums[right + 1])
+                        right -= 1;
+                                    
+                }
+                else if (nums[left] + nums[right] + nums[i] < 0)
+                    left += 1;
+                else
+                    right -= 1;
+                
+            }
+        }
+        
+       
+        return ans; 
+    }
+};
+
