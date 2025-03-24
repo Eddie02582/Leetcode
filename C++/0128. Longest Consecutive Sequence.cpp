8c952c6c8433eaac212aca02f25b0606bc0024e7
02852 <eddie_chuang@sercomm.com>
2025-03-24 09:33:21 +0800

    Update 0128. Longest Consecutive Sequence.cpp

--- C++/0128. Longest Consecutive Sequence.cpp
+++ C++/0128. Longest Consecutive Sequence.cpp
@@ -4,6 +4,22 @@ using namespace std;
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+		int ret = 0;
+		
+		for(int num:nums){
+			// 如果 num-1 不在 numSet 中，说明 num 是一个序列的起点
+			if(!numSet.count(num - 1)){
+				int curr = num;
+				while(numSet.count(curr)){					
+					curr++;
+				}
+				ret = max(ret,curr - num);
+			}			
+		}
+		return ret;
+    }
+    int longestConsecutive(vector<int>& nums) {
         unordered_map<int, int> h;
         int ans = 0;
         for (int num : nums) {
